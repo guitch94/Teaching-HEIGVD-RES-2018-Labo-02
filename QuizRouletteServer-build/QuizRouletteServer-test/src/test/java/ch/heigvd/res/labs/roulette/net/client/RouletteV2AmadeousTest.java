a523cf25c14e49cd0083ada56cf4120be8d46b59
Schnoudli <leo.cortes@heig-vd.ch>
2018-04-03 20:52:33 +0200

    A few tests implemented

--- QuizRouletteServer-build/QuizRouletteServer-test/src/test/java/ch/heigvd/res/labs/roulette/net/client/RouletteV2AmadeousTest.java
+++ QuizRouletteServer-build/QuizRouletteServer-test/src/test/java/ch/heigvd/res/labs/roulette/net/client/RouletteV2AmadeousTest.java
@@ -1,5 +1,6 @@
 package ch.heigvd.res.labs.roulette.net.client;
 
+import ch.heigvd.res.labs.roulette.data.EmptyStoreException;
 import ch.heigvd.res.labs.roulette.net.protocol.RouletteV2Protocol;
 import ch.heigvd.schoolpulse.TestAuthor;
 import org.junit.Rule;
@@ -14,7 +15,7 @@ import static org.junit.Assert.*;
  * This class contains automated tests to validate the client and the server
  * implementation of the Roulette Protocol (version 1)
  *
- * @author Julien Biefer et Léo Cortès
+ * @author Julien Biefer (Amadeous) et Léo Cortès (Schnoudli)
  */
 public class RouletteV2AmadeousTest {
 
@@ -26,8 +27,11 @@ public class RouletteV2AmadeousTest {
 
   @Test
   @TestAuthor(githubId = "amadeous")
-  public void sendingClearCommandShouldClearTheServer() {
-
+  public void sendingClearCommandShouldClearTheServer() throws IOException {
+      // As IRouletteV2Client extedns IRouletteV1Client, we can simply cast it
+      IRouletteV2Client client = (IRouletteV2Client)roulettePair.getClient();
+      client.clear();
+      assertEquals(0, client.getNumberOfStudents());
   }
 
   @Test
@@ -38,20 +42,25 @@ public class RouletteV2AmadeousTest {
 
   @Test
   @TestAuthor(githubId = "amadeous")
-  public void askingListOfStudentWhenThereIsNoStudentShouldReturnAnEmptyList() {
+  public void askingListOfStudentWhenThereIsNoStudentShouldReturnAnEmptyList() throws IOException {
+      IRouletteV2Client client = (IRouletteV2Client)roulettePair.getClient();
+      assertEquals(0, client.getNumberOfStudents());
+  }
+
+  @Test
+  @TestAuthor(githubId = "amadeous")
+  public void loadingAStudentShouldBeSuccessful() throws IOException {
 
   }
 
   @Test
   @TestAuthor(githubId = "amadeous")
-  public void loadingAStudentShouldBeSuccessful() {
-
-  }
-
-  @Test
-  @TestAuthor(githubId = "amadeous")
-  public void loadingAStudentShouldIncreaseTheNumberOfStudentStored() {
-
+  public void loadingAStudentShouldIncreaseTheNumberOfStudentStored() throws IOException {
+      IRouletteV2Client client = (IRouletteV2Client)roulettePair.getClient();
+      int beforeLoad = client.getNumberOfStudents();
+      client.loadStudent("Test");
+      int afterLoad = client.getNumberOfStudents();
+      assertTrue(beforeLoad < afterLoad);
   }
 
   @Test
@@ -62,8 +71,15 @@ public class RouletteV2AmadeousTest {
 
   @Test
   @TestAuthor(githubId = "amadeous")
-  public void sendingByeShouldReturnTheCorrectNumberOfSendCommands() {
+  public void sendingByeShouldReturnTheCorrectNumberOfSendCommands() throws IOException, EmptyStoreException {
+      IRouletteV2Client client = (IRouletteV2Client)roulettePair.getClient();
+      client.getNumberOfStudents();
+      client.getProtocolVersion();
+      client.pickRandomStudent();
+      client.disconnect();
 
+      // Uses the additional method getNumberOfCommands as suggested in the RES 2018 - Annonces Telegram Channel
+      assertEquals(4, client.getNumberOfCommands());
   }
 
   @Test
@@ -82,7 +98,7 @@ public class RouletteV2AmadeousTest {
   @TestAuthor(githubId = "amadeous")
   // This is an adaptation of the same test for v1
   public void theServerShouldReturnTheCorrectVersionNumber() throws IOException {
-    assertEquals(RouletteV2Protocol.VERSION, roulettePair.getClient().getProtocolVersion());
+    assertEquals(RouletteV2Protocol.VERSION, ((IRouletteV2Client)roulettePair.getClient()).getProtocolVersion());
   }
   
 }
