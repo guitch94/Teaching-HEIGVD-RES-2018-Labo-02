ac2bbb3e4bef2be1288f12ae2b95717ffb764469
Schnoudli <leo.cortes@heig-vd.ch>
2018-04-04 17:45:05 +0200

    Some more tests

--- QuizRouletteServer-build/QuizRouletteServer-test/src/test/java/ch/heigvd/res/labs/roulette/net/client/RouletteV2AmadeousTest.java
+++ QuizRouletteServer-build/QuizRouletteServer-test/src/test/java/ch/heigvd/res/labs/roulette/net/client/RouletteV2AmadeousTest.java
@@ -19,85 +19,104 @@ import static org.junit.Assert.*;
  */
 public class RouletteV2AmadeousTest {
 
-  @Rule
-  public ExpectedException exception = ExpectedException.none();
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
 
-  @Rule
-  public EphemeralClientServerPair roulettePair = new EphemeralClientServerPair(RouletteV2Protocol.VERSION);
+    @Rule
+    public EphemeralClientServerPair roulettePair = new EphemeralClientServerPair(RouletteV2Protocol.VERSION);
 
-  @Test
-  @TestAuthor(githubId = "amadeous")
-  public void sendingClearCommandShouldClearTheServer() throws IOException {
-      // As IRouletteV2Client extedns IRouletteV1Client, we can simply cast it
-      IRouletteV2Client client = (IRouletteV2Client)roulettePair.getClient();
-      client.clear();
-      assertEquals(0, client.getNumberOfStudents());
-  }
+    @Test
+    @TestAuthor(githubId = "amadeous")
+    public void sendingClearCommandShouldClearTheServer() throws IOException {
+        // As IRouletteV2Client extedns IRouletteV1Client, we can simply cast it
+        IRouletteV2Client client = (IRouletteV2Client)roulettePair.getClient();
+        client.loadStudent("Test");
+        client.clearDataStore();
+        assertEquals(0, client.getNumberOfStudents());
+    }
 
-  @Test
-  @TestAuthor(githubId = "amadeous")
-  public void askingListOfStudentShouldReturnACorrectList() {
+    @Test
+    @TestAuthor(githubId = "amadeous")
+    public void askingListOfStudentShouldReturnACorrectList() throws IOException {
+        IRouletteV2Client client = (IRouletteV2Client)roulettePair.getClient();
+        client.loadStudent("Test1");
+        client.loadStudent("Test2");
+        client.loadStudent("Test3");
 
-  }
+        assertEquals("Test1", client.listStudents().get(0).getFullname());
+        assertEquals("Test2", client.listStudents().get(0).getFullname());
+        assertEquals("Test3", client.listStudents().get(0).getFullname());
+    }
 
-  @Test
-  @TestAuthor(githubId = "amadeous")
-  public void askingListOfStudentWhenThereIsNoStudentShouldReturnAnEmptyList() throws IOException {
-      IRouletteV2Client client = (IRouletteV2Client)roulettePair.getClient();
-      assertEquals(0, client.getNumberOfStudents());
-  }
+    @Test
+    @TestAuthor(githubId = "amadeous")
+    public void askingListOfStudentWhenThereIsNoStudentShouldReturnAnEmptyList() throws IOException {
+        IRouletteV2Client client = (IRouletteV2Client)roulettePair.getClient();
+        assertEquals(0, client.getNumberOfStudents());
+        assertEquals(0, client.listStudents().size());
+    }
 
-  @Test
-  @TestAuthor(githubId = "amadeous")
-  public void loadingAStudentShouldBeSuccessful() throws IOException {
+    @Test
+    @TestAuthor(githubId = "amadeous")
+    public void loadingAStudentShouldBeSuccessful() throws IOException {
+        IRouletteV2Client client = (IRouletteV2Client)roulettePair.getClient();
+        int actualNb = client.getNumberOfStudents();
+        client.loadStudent("Test");
 
-  }
+        assertEquals("Test", client.listStudents().get(actualNb).getFullname());
+    }
 
-  @Test
-  @TestAuthor(githubId = "amadeous")
-  public void loadingAStudentShouldIncreaseTheNumberOfStudentStored() throws IOException {
-      IRouletteV2Client client = (IRouletteV2Client)roulettePair.getClient();
-      int beforeLoad = client.getNumberOfStudents();
-      client.loadStudent("Test");
-      int afterLoad = client.getNumberOfStudents();
-      assertTrue(beforeLoad < afterLoad);
-  }
+    @Test
+    @TestAuthor(githubId = "amadeous")
+    public void loadingAStudentShouldIncreaseTheNumberOfStudentStored() throws IOException {
+        IRouletteV2Client client = (IRouletteV2Client)roulettePair.getClient();
+        int beforeLoad = client.getNumberOfStudents();
+        client.loadStudent("Test");
+        int afterLoad = client.getNumberOfStudents();
+        assertTrue(beforeLoad < afterLoad);
+    }
 
-  @Test
-  @TestAuthor(githubId = "amadeous")
-  public void sendingByeShouldBeSuccessful() {
+    @Test
+    @TestAuthor(githubId = "amadeous")
+    public void sendingByeShouldBeSuccessful() {
 
-  }
+    }
 
-  @Test
-  @TestAuthor(githubId = "amadeous")
-  public void sendingByeShouldReturnTheCorrectNumberOfSendCommands() throws IOException, EmptyStoreException {
-      IRouletteV2Client client = (IRouletteV2Client)roulettePair.getClient();
-      client.getNumberOfStudents();
-      client.getProtocolVersion();
-      client.pickRandomStudent();
-      client.disconnect();
+    @Test
+    @TestAuthor(githubId = "amadeous")
+    public void sendingByeShouldReturnTheCorrectNumberOfSendCommands() throws IOException, EmptyStoreException {
+        IRouletteV2Client client = (IRouletteV2Client)roulettePair.getClient();
+        client.getNumberOfStudents();
+        client.getProtocolVersion();
+        client.pickRandomStudent();
+        client.disconnect();
 
-      // Uses the additional method getNumberOfCommands as suggested in the RES 2018 - Annonces Telegram Channel
-      assertEquals(4, client.getNumberOfCommands());
-  }
+        // Uses the additional method getNumberOfCommands as suggested in the RES 2018 - Annonces Telegram Channel
+        assertEquals(4, client.getNumberOfCommands());
+    }
 
-  @Test
-  @TestAuthor(githubId = "amadeous")
-  public void sendingInfoShouldBeSuccessful() {
+    @Test
+    @TestAuthor(githubId = "amadeous")
+    public void sendingInfoShouldBeSuccessful() {
 
-  }
+    }
 
-  @Test
-  @TestAuthor(githubId = "amadeous")
-  public void sendingInfoShouldReturnTheCorrectNumberOfStoredStudents() {
-  }
+    @Test
+    @TestAuthor(githubId = "amadeous")
+    public void sendingInfoShouldReturnTheCorrectNumberOfStoredStudents() {
+    }
 
-  @Test
-  @TestAuthor(githubId = "amadeous")
-  // This is an adaptation of the same test for v1
-  public void theServerShouldReturnTheCorrectVersionNumber() throws IOException {
-    assertEquals(RouletteV2Protocol.VERSION, ((IRouletteV2Client)roulettePair.getClient()).getProtocolVersion());
-  }
+    @Test
+    @TestAuthor(githubId = "amadeous")
+    // This is an adaptation of the same test for v1
+    public void theServerShouldReturnTheCorrectVersionNumber() throws IOException {
+        assertEquals(RouletteV2Protocol.VERSION, ((IRouletteV2Client)roulettePair.getClient()).getProtocolVersion());
+    }
+
+    @Test
+    @TestAuthor(githubId = "amadeous")
+    public void theServerShouldReturnTheCorrectDefaultPort() throws IOException {
+        assertEquals(2613, RouletteV2Protocol.DEFAULT_PORT );
+    }
 
 }
